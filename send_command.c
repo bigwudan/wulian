2339c78e71087712186e14aa489a4e982b8e38cd
bigwudan <bigwudan@qq.com>
2018-05-16 14:41:51 +0800

    111

--- send_command.c
+++ send_command.c
@@ -18,6 +18,99 @@
  #define DINADDR_ANY "180.101.147.89" 
 
 
+
+int send_command(char *ptaken)
+{
+
+    int i,j,sockfd, len, fd, size, flag;  
+    char fileName[50],sendFN[20];  
+    struct sockaddr_in dest;  
+    char buffer[MAXBUF + 1];  
+    SSL_METHOD *meth;
+    SSL_CTX *ctx;  
+    SSL *ssl;  
+    char pwd[100];
+    char* temp;
+
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    meth=SSLv23_client_method();
+    ctx=SSL_CTX_new(meth);
+    if(!ctx){
+        exit(1);
+    }
+    /* 创建一个 socket 用于 tcp 通信 */  
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)  
+    {  
+    }  
+    printf("socket created\n");  
+
+    /* 初始化服务器端（对方）的地址和端口信息 */  
+    bzero(&dest, sizeof(dest));  
+    dest.sin_family = AF_INET;  
+    dest.sin_port = htons(8743);  
+    if (inet_aton("180.101.147.89", (struct in_addr *) &dest.sin_addr.s_addr) == 0)  
+    {  
+    }  
+    printf("address created\n");  
+
+    /* 连接服务器 */  
+    if (connect(sockfd, (struct sockaddr *) &dest, sizeof(dest)) != 0)  
+    {  
+
+    }  
+
+    if (SSL_CTX_use_certificate_file(ctx,"client.pem", SSL_FILETYPE_PEM) <= 0)  
+    {  
+    }  
+
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "clientkey.pem", SSL_FILETYPE_PEM) <= 0)  
+    {  
+    }
+
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sockfd);
+
+
+  flag=  SSL_connect(ssl);
+    printf("flag=%d\n", flag);
+    ssize_t size_len = 0;
+    char buff[1000];
+    strcat(buff, "POST /iocm/app/cmd/v1.3.0/devices/ede76b44-7823-40f8-a258-a0c68f2fd8c9/commands HTTP/1.1\r\n");
+    strcat(buff, "Host: 180.101.147.89:8743\r\n");
+    strcat(buff, "Accept: */*\r\n");
+    strcat(buff, "app_key: Xqwyn4lJPuLIPYqHBH8UN_zK8fsa\r\n");
+    strcat(buff, "Authorization: ");
+    strcat(buff, ptaken);
+    strcat(buff, "\r\n");
+
+
+
+    strcat(buff, "Content-type: application/json\r\n");
+    strcat(buff, "Content-Length: 85\r\n\r\n");
+    strcat(buff, "{\"command\":{\"serviceId\":\"LOCK\",\"method\":\"cmd\",\"paras\":{\"cmd01\":\"77\"}},\"expireTime\":0}");
+
+    len = SSL_write(ssl, buff, 1000);
+    char buffer_len[2024];
+    /* 接收服务器来的消息*/
+    len = SSL_read(ssl, buffer_len, 2024);
+    if(len > 0 ){
+        printf("接收消息成功:'%s'，共%d 个字节的数据\n",
+               buffer_len, len);
+    }
+
+    return 1;
+
+
+
+}
+
+
+
+
 int main(){
 
 	int i,j,sockfd, len, fd, size, flag;  
@@ -128,39 +221,13 @@ char *p_appkey = "XIcGewffJE8LpfMWksO4Vipvfsga";
     p_start =  strstr(p_add, "\"");
     dur = p_start- p_add;
     memcpy(taken, p_add, dur); 
-
     printf("****************/r/n");
-
-
-
-
-
-
-
-    char buff_com[1000]={0};
-    strcat(buff_com, "POST /iocm/app/cmd/v1.3.0/devices/ede76b44-7823-40f8-a258-a0c68f2fd8c9/commands HTTP/1.1\r\n");
-    strcat(buff_com, "Host: 180.101.147.89:8743\r\n");
-    strcat(buff_com, "Accept: */*\r\n");
-    strcat(buff_com, "app_key: Xqwyn4lJPuLIPYqHBH8UN_zK8fsa\r\n");
-    strcat(buff_com, "Authorization: ");
-
-    strcat(buff_com, taken);
-    strcat(buff_com, "\r\n");
-
-    strcat(buff_com, "Content-type: application/json\r\n");
-    strcat(buff_com, "Content-Length: 85\r\n\r\n");
-    strcat(buff_com, "{\"command\":{\"serviceId\":\"LOCK\",\"method\":\"cmd\",\"paras\":{\"cmd01\":\"77\"}},\"expireTime\":0}");
-
-    len = SSL_write(ssl, buff_com, 1000);
-    char buffer_len_com[2024] = {0};
-
-    /* 接收服务器来的消息*/
-    len = SSL_read(ssl, buffer_len_com, 2024);
-
-    
-    printf("len=%d\n", len);
+    printf("token=%s\n", taken);
+    close(sockfd);
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+    send_command(taken);
     exit(1);
-
-	return 1;
 }
 
